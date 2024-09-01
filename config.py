3df3810efc14b6720d9550bfbb8e1dbea85ecf3d
Navneetnameless <navneetsuppin12345@gmail.com>
2024-09-01 19:36:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1302933634').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002192793396") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002197485186"))
     
